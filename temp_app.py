b5ad2b885f6b2008593ccb71a300eb2e70fe24bf
boranozum <boranozum@gmail.com>
2022-03-23 21:01:51 +0300

    Added login and register pages

--- temp_app.py
+++ temp_app.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template, url_for
+from crypt import methods
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config["SECRET_KEY"] = "43b188b91f7285ba79d22dc5485e8cdf"
 
 posts = [
     {
@@ -27,6 +31,30 @@ def home():
 def about():
     return render_template('about.html', title='About')
 
+
+@app.route("/register", methods=["GET","POST"])
+def register():
+    form = RegistrationForm()
+
+    if form.validate_on_submit():
+        flash(f"Account created for {form.username.data}!", "success")
+        return redirect(url_for("home"))
+    
+    return render_template("register.html",title="Register",form=form)
+
+@app.route("/login", methods=["GET","POST"])
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        if form.email.data == "admin@blog.com" and form.password.data == "password":
+            flash("Login successful!","success")
+            return redirect(url_for("home"))
+        else:
+            flash("Login unsuccessful. Please check your credentials.","danger")
+
+    
+    return render_template("login.html",title="Login",form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
